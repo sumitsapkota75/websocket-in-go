35b7caeb9e255b3a504f0e86081e6a97435cea02
Sumit <sumitsapkota0@gmail.com>
2021-07-16 14:56:12 +0545

    added upgrader for connection

--- main.go
+++ main.go
@@ -10,8 +10,19 @@ func Homepage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home page for websockets")
 }
 
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
 func SetupRoutes() {
 	http.HandleFunc("/", Homepage)
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	// remove the previous fmt statement
+	// fmt.Fprintf(w, "Hello World")
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
 }
 
 func main() {
---
+++ upgrader.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/gorilla/websocket"
+
+// We'll need to define an Upgrader
+// this will require a Read and Write buffer size
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
